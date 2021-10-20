cd4fb8f26422239cd7ebfc37852c5c903dd5e851
csaundersshultz <67344206+csaundersshultz@users.noreply.github.com>
2021-10-20 13:04:15 -0800

    Add files via upload

---
+++ labs/plot_gps_data.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import sys
+import matplotlib.pyplot as plt
+from datetime import datetime
+from numpy    import genfromtxt
+
+filename = sys.argv[1]
+site     = filename[:4]
+
+data     = genfromtxt(filename, encoding='utf-8', dtype=None, delimiter=',', skip_header=12, names=['date', 'north', 'east', 'vertical', 'nStd', 'eStd', 'vStd', 'quality', 'dummy'])
+dates    = [datetime.strptime(x, '%Y-%m-%d') for x in data['date']]
+
+plt.subplot(3,1,1)
+plt.errorbar(dates, data['north'], yerr=data['nStd'], fmt='.', ecolor='gray')
+plt.ylabel('north displacement (mm)')
+plt.title('site %s from %s' % (site, filename))
+
+plt.subplot(3,1,2)
+plt.errorbar(dates, data['east'], yerr=data['eStd'], fmt='.', ecolor=[0.5,0.5,0.5])
+plt.ylabel('east (mm)')
+
+plt.subplot(3,1,3)
+plt.errorbar(dates, data['vertical'], yerr=data['vStd'], fmt='.', ecolor='0.5')
+plt.ylabel('vertical (mm)')
+plt.xlabel('date')
+
+plt.tight_layout()
+
+print("Saving figure '"+filename+".png'")
+plt.savefig(filename+'.png', bbox_inches='tight')
+
