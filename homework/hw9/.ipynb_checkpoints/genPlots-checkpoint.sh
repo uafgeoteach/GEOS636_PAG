b6d868b01830bc1a74509401bbe8deb734a297f5
Ronni Grapenthin <grapenthin.research@gmail.com>
2026-08-27 16:57:10 -0800

    Delete homework directory

--- homework/hw9/.ipynb_checkpoints/genPlots-checkpoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-FILES="./*.csv"
-for f in $FILES
-do
-	python speedPlot.py $f
-done
--- homework/hw9/.ipynb_checkpoints/test-checkpoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#changing directories. NOTE: This script will break if files are moved to a different directory
-for file in ./*.csv; do python speedPlot.py $file; done
-#iterating over csv files in directory
